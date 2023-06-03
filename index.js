164f6169819d119cb87ec2cc7d12ef224bb9f9e3
Piero <piero.suarez.zuniga@alumnos.uta.cl>
2023-06-03 19:51:52 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const PeerServer = require('peer').PeerServer;
+const server = PeerServer({
+  port: 9000,
+  path: '/myapp',
+  allow_discovery: true,
+  key: 'peerjs', // Agregamos esta línea
+  // Otras opciones de configuración...
+});
+
+server.on('connection', (client) => {
+  console.log(`Cliente conectado: ${client.getId()}`);
+});
+
+server.on('disconnect', (client) => {
+  console.log(`Cliente desconectado: ${client.getId()}`);
+});
